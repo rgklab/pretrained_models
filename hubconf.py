9f85cc9ec396e7ac33ec9f78799712b31c4a9586
TomGinsberg <tomginsberg@voyager.ais.sandbox>
2023-02-11 13:48:38 -0500

    updated hubconf

--- hubconf.py
+++ hubconf.py
@@ -1,7 +1,10 @@
 import torch
+from os.path import join
+
+dirname = os.path.dirname(__file__)
 
 def resnet18_cifar10(return_transform=True):
-    model = torch.load('detectron/cifar10.pt')
+    model = torch.load(join(dirname, 'detectron/cifar10.pt'))
     if return_transform:
-        return model, torch.load('detectron/cifar10_input_transform.pt')
+        return model, torch.load(join(dirname, 'detectron/cifar10_input_transform.pt'))
     return model
